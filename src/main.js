e5ddf379cf0407dc59ba2df6ba0aa225ea202fc6
Bassem Kaddour <bassemkaddour@gmail.com>
2018-11-18 09:47:33 +0000

    Added player to map

--- src/main.js
+++ src/main.js
@@ -1,22 +1,32 @@
 import TitleScene from './scenes/TitleScene';
-import L1Scene from './scenes/L1Scene';
+import WorldScene from './scenes/WorldScene';
 import BootScene from './scenes/BootScene';
 import LoadingScene from './scenes/LoadingScene';
 
 let titleScene = new TitleScene();
-let l1Scene = new L1Scene();
+let worldScene = new WorldScene();
 let bootScene = new BootScene();
 let loadingScene = new LoadingScene();
 
 let config = {
     type: Phaser.AUTO,
-    width: 640,
-    height: 640
+    width: 500,
+    height: 300,
+    parent: 'game-container',
+    zoom: 2,
+    pixelArt: true,
+    roundPixels: true,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: {y: 0}
+        }
+    }
 };
 
 let game = new Phaser.Game(config);
 game.scene.add('TitleScene', titleScene);
-game.scene.add('L1Scene', l1Scene);
+game.scene.add('WorldScene', worldScene);
 game.scene.add('BootScene', bootScene);
 game.scene.add('LoadingScene', loadingScene);
 game.scene.start('BootScene', {scene: 'title'});
