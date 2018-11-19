f8552a044f8391d37af863c4f945b07b7d592218
Bassem Kaddour <bassemkaddour@gmail.com>
2018-11-19 23:09:17 +0000

    Add functioning townhall map'

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -6,7 +6,7 @@ class BootScene extends Phaser.Scene {
             title: {key: 'TitleScene', path: 'assets/levels/title_screen.json'},
             // level1: {key: 'L1Scene', path: 'assets/levels/level1.json'},
             mayor: {key: 'MayorScene', path: 'assets/levels/mayor.json'},
-            town: {key: 'WorldScene', path: 'assets/levels/townhall.json'}
+            townhall: {key: 'WorldScene', path: 'assets/levels/townhall.json'}
         };
     }
     
