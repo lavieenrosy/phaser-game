e9fc633595f709f34f4d96fc67d3e1ca9c81c848
Daniel Escobedo <danesco30@gmail.com>
2018-11-20 04:07:40 +0000

    add enter key to remove dialogue box

--- src/prefabs/world/NPC.js
+++ src/prefabs/world/NPC.js
@@ -1,5 +1,6 @@
 import Prefab from '../Prefab';
 import MessageBox from '../HUD/MessageBox';
+import Player from './Player'
 
 export default class NPC extends Prefab {
   constructor (scene, name, position, properties) {
@@ -7,9 +8,10 @@ export default class NPC extends Prefab {
 
     this.message = this.scene.cache.text.get(properties.message);
 
+    console.log(this.x, this.y);
     this.body.immovable = true;
 
-    this.Message_box_position = {x: 0, y: 360};
+    this.Message_box_position = {x: this.x - 300, y: this.y + 90};
 
     this.scene.physics.add.collider(this, this.scene.groups.players, this.talk, null, this);
 
@@ -19,8 +21,11 @@ export default class NPC extends Prefab {
   talk(npc, player) {
     player.stop();
     console.log(this.message);
-
     //create a new message box and pass the npc message property as the text
-    this.scene.current_message_box = new MessageBox(this.scene, this.name + '_message_box', this.Message_box_position, {texture: 'message_box_image', group: 'hud', message: this.message});
+    if (!(this.scene.current_message_box)){
+      this.scene.current_message_box = new MessageBox(this.scene, this.name + '_message_box', this.Message_box_position, {texture: 'message_box_image', group: 'hud', message: this.message});
+
+      console.log('this2', this.scene.current_message_box)
+    }
   }
 }
