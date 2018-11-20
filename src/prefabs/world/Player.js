1429c0bf322b50bec758e4506df683166683e25d
Andrea <andreamayhirji@gmail.com>
2018-11-20 10:18:58 +0000

    adds new tilesets with the revised tilemap spritesheet, adds collision for water and buildings related layers to the Player.js

--- src/prefabs/world/Player.js
+++ src/prefabs/world/Player.js
@@ -9,6 +9,9 @@ class Player extends Prefab {
         this.body.collideWorldBounds = true;
         
         this.scene.physics.add.collider(this, this.scene.layers.buildings);
+        this.scene.physics.add.collider(this, this.scene.layers.water);
+        this.scene.physics.add.collider(this, this.scene.layers.above_buildings);       
+        this.scene.physics.add.collider(this, this.scene.layers.beneath_buildings);        
         
         this.body.velocity.x = -this.walking_speed;
 
