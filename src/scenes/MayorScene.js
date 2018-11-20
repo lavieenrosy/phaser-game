608dbe44dfb68b156838cb8b671965ea00a77f88
Rosy Lee <rosyhlee@gmail.com>
2018-11-20 04:54:50 +0000

    added typewriter file

--- src/scenes/MayorScene.js
+++ src/scenes/MayorScene.js
@@ -2,27 +2,44 @@ import JSONLevelScene from './JSONLevelScene';
 // import Prefab from '../prefabs/Prefab.js';
 // import TextPrefab from '../prefabs/TextPrefab.js';
 import Player2 from '../prefabs/Player2.js';
+import Typewriter from '../prefabs/HUD/Typewriter.js'
 
 class MayorScene extends Phaser.Scene {
+
   constructor() {
     super('MayorScene');
   }
 
   preload() {
     this.load.spritesheet('characters', 'assets/images/world/modern5.png', { frameWidth: 46, frameHeight: 64 });
+
   }
 
   create() {
     let player1 = new Player2(this, 180, 150, 'characters', 31, 2);
     let player2 = new Player2(this, 500, 150, 'characters', 64, 2);
+
   }
 
   update() {
     let enterKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
     if (enterKey.isDown) {
-      this.start_game();
+      const typewriter = new Typewriter();
+      console.log(typewriter);
+      typewriter.init(this.scene, {
+        x: 290,
+        y: 40,
+        fontFamily: "chillerBlack",
+        fontSize: 26,
+        maxWidth: 300,
+        text: "Some text to be typed!"
+      });
+
+      // this.start_game();
     };
   }
+
+
 
   start_game() {
       this.scene.start('BootScene', {scene: 'level1'});
