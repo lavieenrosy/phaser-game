c46a4d41d5aab0341baf005b23a04a670c638ef3
Andrea <andreamayhirji@gmail.com>
2018-11-22 00:53:06 +0000

    testing typewriter.js, no changes.

--- src/prefabs/Typewriter.js
+++ src/prefabs/Typewriter.js
@@ -1,13 +1,14 @@
 // set up text to print, each item in array is new line
-var aText = new Array(
+var aText = [
     "Sample Text",
     "Sample Sample Text",
     "Sample Sample Sample Text"
-    );
+]
+
     var iSpeed = 100; // time delay of print out
     var iIndex = 0; // start printing array at this posision
     var iArrLength = aText[0].length; // the length of the text array
-    var iScrollAt = 20; // start scrolling up at this many lines
+    var iScrollAt = 204; // start scrolling up at this many lines
      
     var iTextPos = 0; // initialise text position
     var sContents = ''; // initialise contents variable
@@ -37,7 +38,6 @@ var aText = new Array(
 
     
     typewriter();
-
 
 
 
--- src/scenes/MayorScene.js
+++ src/scenes/MayorScene.js
@@ -45,7 +45,45 @@ Speaking as the previous mayor, let me give you one valuable piece of advice: ch
     // this.runAnimation(10);
     // this.mainChar.anims.play('character');
 // set up text to print, each item in array is new line
+var aText = [
+  "Sample Text",
+  "Sample Sample Text",
+  "Sample Sample Sample Text"
+  ]
 
+  var iSpeed = 100; // time delay of print out
+  var iIndex = 0; // start printing array at this posision
+  var iArrLength = aText[0].length; // the length of the text array
+  var iScrollAt = 204; // start scrolling up at this many lines
+   
+  var iTextPos = 0; // initialise text position
+  var sContents = ''; // initialise contents variable
+  var iRow; // initialise current row
+   
+  function typewriter()
+  {
+   sContents =  ' ';
+   iRow = Math.max(0, iIndex-iScrollAt);
+   var destination = document.getElementById("typedtext");
+   while ( iRow < iIndex ) {
+    sContents += aText[iRow++] + '<br />';
+   }
+   destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) ; 
+
+   if ( iTextPos++ == iArrLength ) {
+    iTextPos = 0;
+    iIndex++;
+    if ( iIndex != aText.length ) {
+     iArrLength = aText[iIndex].length;
+     setTimeout("typewriter()", 500);
+    }
+   } else {
+    setTimeout("typewriter()", iSpeed);
+   }
+  }
+
+  
+  typewriter();
 
     this.addMessage(message.level1);
 
