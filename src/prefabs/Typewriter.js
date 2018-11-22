9fa29bd130bc8fc5de0a0a31c441a10cb72c21df
Rosy Lee <rosyhlee@gmail.com>
2018-11-22 06:14:56 +0000

    fixed small syntax bug in mayorscene

--- src/prefabs/Typewriter.js
+++ src/prefabs/Typewriter.js
@@ -8,11 +8,11 @@ var aText = new Array(
     var iIndex = 0; // start printing array at this posision
     var iArrLength = aText[0].length; // the length of the text array
     var iScrollAt = 20; // start scrolling up at this many lines
-     
+
     var iTextPos = 0; // initialise text position
     var sContents = ''; // initialise contents variable
     var iRow; // initialise current row
-     
+
     function typewriter()
     {
      sContents =  ' ';
@@ -21,7 +21,7 @@ var aText = new Array(
      while ( iRow < iIndex ) {
       sContents += aText[iRow++] + '<br />';
      }
-     destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) ; 
+     destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) ;
 
      if ( iTextPos++ == iArrLength ) {
       iTextPos = 0;
@@ -35,8 +35,8 @@ var aText = new Array(
      }
     }
 
-    
-    typewriter();
+
+    // typewriter();
 
 
 
