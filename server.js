2ddfe8d8126116a28c5a6dce64ad46106c97752d
Bassem Kaddour <bassemkaddour@Bassems-MacBook-Pro.local>
2018-11-23 13:52:11 -0800

    Update server message

--- server.js
+++ server.js
@@ -12,5 +12,5 @@ app.get('*', (req, res) => {
 });
 
 app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  console.log("Express server listening on port", this.address().port);
 });
