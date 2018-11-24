4b64b52a97e1130dc0dd1990539e949aed56a2c2
Andrea <andreamayhirji@gmail.com>
2018-11-24 00:11:57 +0000

    tests console.logs

--- dist/bundle.js
+++ dist/bundle.js
@@ -4982,7 +4982,7 @@ class Player extends __WEBPACK_IMPORTED_MODULE_0__Prefab__["a" /* default */] {
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(/*! babel-polyfill */558);
-module.exports = __webpack_require__(/*! /Users/bassemkaddour/lighthouse/final/phaser-game/src/main.js */760);
+module.exports = __webpack_require__(/*! /Users/andreahirji/lighthouse/phaser-game/src/main.js */760);
 
 
 /***/ }),
@@ -12283,6 +12283,8 @@ class StaticPlayer extends Phaser.Physics.Arcade.Sprite {
 class TownhallScene extends __WEBPACK_IMPORTED_MODULE_0__WorldScene__["a" /* default */] {
   constructor() {
     super('TownhallScene');
+    this.money = 1000000;
+    this.popularity = 50;
   }
 
   create(data) {
@@ -12292,16 +12294,10 @@ class TownhallScene extends __WEBPACK_IMPORTED_MODULE_0__WorldScene__["a" /* def
     this.optionA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
     this.optionB = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.B);
     this.optionC = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.C);
-    this.optionD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
 
-    if (data.next_level === 'level2') {
-      this.chosen_scene = 'level3';
-    } else if (data.next_level === 'level3') {
-      this.chosen_scene = 'level4';
-    } else {
-      this.chosen_scene = 'level2';
-    }
+    this.setScene(data.next_level);
   }
+
   update() {
 
     super.update();
@@ -12310,26 +12306,102 @@ class TownhallScene extends __WEBPACK_IMPORTED_MODULE_0__WorldScene__["a" /* def
     if (messageBox) {
       if (this.optionA.isDown) {
         messageBox.remove();
-        this.next_level(this.chosen_scene);
+        this.choice = 'A';
+        this.calculatePoints(this.data.next_level);
+        this.sendPoints();
+        this.startScene(this.next_level);
       } else if (this.optionB.isDown) {
         messageBox.remove();
-        this.next_level(this.chosen_scene);
+        this.choice = 'B';
+        this.calculatePoints(this.data.next_level);
+        this.sendPoints();
+        this.startScene(this.next_level);
       } else if (this.optionC.isDown) {
         messageBox.remove();
-        this.next_level(this.chosen_scene);
-      } else if (this.optionD.isDown) {
-        messageBox.remove();
-        this.next_level(this.chosen_scene);
+        this.choice = 'C';
+        this.calculatePoints(this.data.next_level);
+        this.sendPoints();
+        this.startScene(this.next_level);
       }
     }
   }
-  next_level(level) {
-    if (this.data && this.data.next_level === 'level3') {
-      this.scene.start('BootScene', { scene: 'level4' });
+
+  setScene(currentLevel) {
+    if (currentLevel === 'level2') {
+      this.next_level = 'level3';
+    } else if (currentLevel === 'level3') {
+      this.next_level = 'level4';
     } else {
-      this.scene.start('BootScene', { scene: 'mayor', level: level });
+      this.next_level = 'level2';
     }
   }
+
+  calculatePoints(currentLevel) {
+    if (currentLevel === 'level2') {
+      switch (this.choice) {
+        case 'A':
+          this.money += 125000;
+          this.popularity -= 20;
+          break;
+        case 'B':
+          this.money += 100000;
+          this.popularity += 0;
+          break;
+        case 'C':
+          this.money += 3000;
+          this.popularity += 25;
+          break;
+      }
+    } else if (currentLevel === 'level3') {
+      switch (this.choice) {
+        case 'A':
+          this.money += 125000;
+          this.popularity -= 20;
+          break;
+        case 'B':
+          this.money += 100000;
+          this.popularity += 0;
+          break;
+        case 'C':
+          this.money += 3000;
+          this.popularity += 25;
+          break;
+      }
+    } else {
+      switch (this.choice) {
+        case 'A':
+          this.money += 125000;
+          this.popularity -= 20;
+          break;
+        case 'B':
+          this.money += 100000;
+          this.popularity += 0;
+          break;
+        case 'C':
+          this.money += 3000;
+          this.popularity += 25;
+          break;
+      }
+    }
+  }
+
+  sendPoints() {
+    console.log("this.money: ", this.money);
+    console.log("this.popularity: ", this.popularity);
+  }
+
+  startScene(level) {
+    this.cameras.main.fade(700, 0, 0, 0);
+    this.cameras.main.on('camerafadeoutcomplete', () => {
+      if (this.data && this.data.next_level === 'level3') {
+        this.scene.start('BootScene', { scene: 'level4', money: this.money, popularity: this.popularity });
+      } else {
+        this.scene.start('BootScene', { scene: 'mayor', level: level, money: this.money, popularity: this.popularity });
+      }
+      console.log('townhall level data:', level);
+    });
+  }
+
 }
 
 /* harmony default export */ __webpack_exports__["a"] = (TownhallScene);
@@ -12430,6 +12502,7 @@ class BootScene extends Phaser.Scene {
   init(data) {
     if (data.level) {
       this.next_level = data.level;
+      console.log('Data from BootScene:', data);
     }
   }
 
