b3bd7fb6d8d2d9d28751384bd6f02407e14ed20b
Andrea <andreamayhirji@gmail.com>
2018-11-24 20:53:28 +0000

    adds ajax call, score and username rendering to the page.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,5 @@
 $(() => {
   console.log("we are in document ready")
-  loadScores();
 
   function createScoreTableRows(score) {
     
@@ -11,26 +10,16 @@ $(() => {
     const scoreData = $('<td>').addClass('scores__td').text(vscore);
     
     let scoreRow = vrow.append(nameData).append(scoreData);
-    console.log('score row', scoreRow)
 
     return scoreRow;
   }
 
   //iterate through the score data 
   function renderScores(scores){
-    for(let i = 0; i <= scores.length; i ++){
-      $('#scores').prepend(createScoreTableRows(scores[i]))
-    }
+    scores.forEach(function (score) {
+      $('#scores').append(createScoreTableRows(score));
+    });
   }
-
-  // load scores from the server 
-  function loadScores(){
-    // $.ajax('https://bajo-island-api.herokuapp.com/api/users/scores', {method: 'GET'})
-    //   .then(function(score){
-    //     $('#scores').empty();
-    //     renderScores(score);
-    //   });
-    // }
 
     $.ajax({
       url: 'https://bajo-island-api.herokuapp.com/api/users/scores',
@@ -42,7 +31,16 @@ $(() => {
         console.log("we are in error",error);
       }
     });
-  }
  
+  // load scores from the server 
+  function loadScores(){
+    $.get('https://bajo-island-api.herokuapp.com/api/users/scores', function(score) {
+      $('#scores').empty();
+      renderScores(score);
+    });
+  }
+
+  loadScores();
+  
 });
 
