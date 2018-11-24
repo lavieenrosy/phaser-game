b292428709e5681b0cdb5c9948adb2457fbf3e37
Andrea <andreamayhirji@gmail.com>
2018-11-24 18:38:16 +0000

    function for creating score table

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,33 @@
-/* <section id="scores" class="scores">
+$() => {
+
+  function createScoreTable(data) {
+    const name = data.[index].name;
+    const score = data.[index].score;
+
+    const article = $('<article>').addClass('scores__container');
+    const title = $('<h2>').text("HIGH SCORES");
+    const table = $('<table>').addClass('.scores__table');
+    const nameColumn = $('<th>').text("NAME");
+    const scoreColumn = $('<th>').text("SCORE");
+    const nameData = $('<td>').addClass('.scores__td').text(name);
+    const scoreData = $('<td>').addClass('.scores__td').text(score);
+  }
+
+  
+  function loadScoreTable(){
+    $.ajax('https://bajo-island-api.herokuapp.com/api/users/scores').then((response) => {
+      renderScoreTable(response.data)
+    })
+  }
+}
+
+
+
+
+
+
+
+/* <section id="scores" class="scores"> // append to here. this section should stay on the page
 <article id="scores__container" class="scores__container">
     <h2>High Scores</h2>
   <table class="scores__table">
@@ -21,13 +50,3 @@
   </table>
 </article>
 </section> */
-
-
-$(document).ready(function () {
-
-  function createScoreTable(scoreData) {
-    return $('<section>').addClass('.scores')
-  }
-
-
-})
