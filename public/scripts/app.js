921d33fa62ca8cc5f35771b9dd24e8d9a2a62329
Andrea <andreamayhirji@gmail.com>
2018-11-24 19:32:55 +0000

    adds render function to the scores ajax request

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,52 +1,53 @@
-$() => {
-
-  function createScoreTable(data) {
-    const name = data.[index].name;
-    const score = data.[index].score;
-
-    const article = $('<article>').addClass('scores__container');
-    const title = $('<h2>').text("HIGH SCORES");
-    const table = $('<table>').addClass('.scores__table');
-    const nameColumn = $('<th>').text("NAME");
-    const scoreColumn = $('<th>').text("SCORE");
-    const nameData = $('<td>').addClass('.scores__td').text(name);
-    const scoreData = $('<td>').addClass('.scores__td').text(score);
+$(() => {
+  function createScoreTableRows(scoreInfo) {
+    
+    const name = scoreInfo.name;
+    const score = scoreInfo.score;
+    const row = $('<tr>');
+    const nameData = $('<td>').addClass('scores__td').text(name);
+    const scoreData = $('<td>').addClass('scores__td').text(score);
+    
+    let scoreRow = row.append(nameData).append(scoreData);
+    return scoreRow;
   }
 
+  //iterate throught the score data 
+  function renderScores(scores){
+    for(let i = 0; i <= scores.length; i ++){
+      $('#scores').prepend(createScoreTableRows(scores[i]))
+    }
+  }
+
+  // load scores from the server
+  function loadScores(){
+    $.ajax('https://bajo-island-api.herokuapp.com/api/users/scores', {method: 'GET'})
+      .then(function(data){
+        $('#scores').empty();
+        renderScores(data);
+      });
+    }
   
-  function loadScoreTable(){
-    $.ajax('https://bajo-island-api.herokuapp.com/api/users/scores').then((response) => {
-      renderScoreTable(response.data)
-    })
-  }
-}
+    loadScores();
 
+});
 
-
-
-
-
-
-/* <section id="scores" class="scores"> // append to here. this section should stay on the page
-<article id="scores__container" class="scores__container">
-    <h2>High Scores</h2>
-  <table class="scores__table">
-    <tr>
-      <th>SCORE</th>
-      <th>NAME</th>
-    </tr> 
-    <tr>
-      <td class ="scores__td">2150</td>
-      <td class ="scores__td">BASSEM</td>
-    </tr> 
-    <tr>
-      <td class ="scores__td">2050</td>
-      <td class ="scores__td">ANDREA</td>
-    </tr>
-    <tr>
-      <td class ="scores__td">1800</td>
-      <td class ="scores__td">ROSY</td>
-    </tr>
-  </table>
-</article>
-</section> */
+/*     
+<table id="scores" class="scores__table">
+          <tr>
+            <td class="scores__td">2150</td>
+            <td class="scores__td">BASSEM</td>
+          </tr> 
+          <tr>
+            <td class="scores__td">2050</td>
+            <td class="scores__td">ANDREA</td>
+          </tr>
+          <tr>
+            <td class="scores__td">1800</td>
+            <td class="scores__td">ROSY</td>
+          </tr>
+          <tr>
+              <td class="scores__td">1750</td>
+              <td class="scores__td">ANDREA</td>
+            </tr>
+        </table>
+*/
