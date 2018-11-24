d66c843e88a9e8053e1881fd2e5ea00ecd57c8f4
Andrea <andreamayhirji@gmail.com>
2018-11-24 05:10:34 +0000

    adds public/scripts folder, adds app.js, adds script tags to index.html

---
+++ public/scripts/app.js
@@ -0,0 +1,33 @@
+/* <section id="scores" class="scores">
+<article id="scores__container" class="scores__container">
+    <h2>High Scores</h2>
+  <table class="scores__table">
+    <tr>
+      <th>SCORE</th>
+      <th>NAME</th>
+    </tr> 
+    <tr>
+      <td class ="scores__td">2150</td>
+      <td class ="scores__td">BASSEM</td>
+    </tr> 
+    <tr>
+      <td class ="scores__td">2050</td>
+      <td class ="scores__td">ANDREA</td>
+    </tr>
+    <tr>
+      <td class ="scores__td">1800</td>
+      <td class ="scores__td">ROSY</td>
+    </tr>
+  </table>
+</article>
+</section> */
+
+
+$(document).ready(function () {
+
+  function createScoreTable(scoreData) {
+    return $('<section>').addClass('.scores')
+  }
+
+
+})
