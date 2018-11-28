daed4ba384b4716daf5c44be1af9522f1056432c
Rosy Lee <rosyhlee@gmail.com>
2018-11-28 23:12:23 +0000

    fixed bug where you could advance to next scene without messagebox

--- src/scenes/Level4Scene.js
+++ src/scenes/Level4Scene.js
@@ -11,23 +11,20 @@ class Level4Scene extends WorldScene {
   }
 
   update() {
-
     super.update();
 
-    let messageBox = $('#messagebox');
-
+    let messageBox = document.querySelector('#messagebox')
     if (messageBox) {
-      messageBox.css('height', '230px');
+      messageBox.style.height = '230px';
       $('.game__footer').empty().text("press spacebar to continue")
     }
-
     if (messageBox && this.spaceBar.isDown) {
       messageBox.remove();
       this.next_level();
       this.sumbit_score();
     }
-
   }
+
   next_level() {
     this.scene.start('BootScene', {scene: 'gameover'});
   }
@@ -49,24 +46,19 @@ class Level4Scene extends WorldScene {
 
   loadScores(){
     $.get('https://bajo-island-api.herokuapp.com/api/users/scores', (scores) => {
-      // $('#scores').empty();
       for(let i = 0; i < 7; i ++) {
         $('#scores').append(this.createScoreTableRows(scores));
       }
     });
   }
 
-
   createScoreTableRows(score) {
-
     const vname = score.name;
     const vscore = score.scores;
     const vrow = $('<tr>');
     const nameData = $('<td>').addClass('scores__td').text(vname);
     const scoreData = $('<td>').addClass('scores__td').text(vscore);
-
     let scoreRow = vrow.append(nameData).append(scoreData);
-
     return scoreRow;
   }
 }
